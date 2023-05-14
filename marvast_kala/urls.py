80b5907d0e7043d4c6e5b6fa1928ec47e388408c
amirhossein <nikuro-dev>
2023-05-14 04:06:26 +0330

    add home_module & product_module urls to main urls file

--- marvast_kala/urls.py
+++ marvast_kala/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("", include("home_module.urls")),
+    path("products/", include("product_module.urls"))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
